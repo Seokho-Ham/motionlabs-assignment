ecd54344c109bb2770cae05c4c9de65ca1420d6d
SeokHo-Ham <kalejuice02@gmail.com>
2023-03-31 13:44:41 +0900

    initial commit

---
+++ src/main/java/com/motionlabs/MotionlabsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.motionlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionlabsAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotionlabsAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/motionlabs/MotionlabsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motionlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotionlabsAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
