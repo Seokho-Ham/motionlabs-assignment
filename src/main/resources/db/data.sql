efa4b2ebd9e13acad7ff128faa08d33631c611f6
SeokHo-Ham <kalejuice02@gmail.com>
2023-04-04 13:26:26 +0900

    refactor: JPA Auditing 기능 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO MEMBER(id, nickname, email)
-VALUES (1, 'test-user', 'test@email.com');
+INSERT INTO MEMBER(id, nickname, email, created_at)
+VALUES (1, 'test-user', 'test@email.com', now());
